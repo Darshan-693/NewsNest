363439911a105f4022c43f9cad8443881be0ad0d
Darshan-693 <136052457+Darshan-693@users.noreply.github.com>
2024-06-23 23:47:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import Dashboard from './Components/Dashboard'
 import Navbar from  "./Components/Navbar"
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import About from './Components/About'
-
+import Start from "./Components/Start"
 function App() {
   return (
     <>
     <Navbar/>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing/>} />
+        <Route path='/' element={<Start/>} />
+        <Route path='/landing' element={<Landing/>} />
         <Route path='/layout' element={<Dashboard/>} />
         <Route path='/about' element={<About/>} />
       </Routes>
