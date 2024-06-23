7d8802fbb1901bab3845f1e5b818c74ddf039d3a
Darshan-693 <136052457+Darshan-693@users.noreply.github.com>
2024-06-23 23:49:36 +0530

    Update Start.jsx

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function App() {
   return (
     <div className="main">
-      <center><h1>A1 Web Sccrapers</h1></center>
+      <center><h1>News-Nest</h1></center>
       <center><h2>"Automate Your Data Collection, Amplify Your Results."</h2></center>
       <button>Enter</button>
     </div>
