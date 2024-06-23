4f58302ee0ccd1248a450195387d363b482a921f
Darshan-693 <136052457+Darshan-693@users.noreply.github.com>
2024-06-23 23:46:07 +0530

    Add files via upload

---
+++ src/Components/Start.jsx
@@ -0,0 +1,13 @@
+import "../App.css";
+import {Link} from 'react-router-dom';
+function App() {
+  return (
+    <div className="main">
+      <center><h1>A1 Web Sccrapers</h1></center>
+      <center><h2>"Automate Your Data Collection, Amplify Your Results."</h2></center>
+      <button>Enter</button>
+    </div>
+  );
+}
+
+export default App;
